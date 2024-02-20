5d899b17e9cd45771c852961bfbba0443f7f1e2b
kamlesh1specindia <108715861+kamlesh1specindia@users.noreply.github.com>
2024-02-20 15:58:14 +0530

    initial comment

--- toast_library/build.gradle.kts
+++ toast_library/build.gradle.kts
@@ -1,15 +1,13 @@
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("maven-publish")
 }
 
-group = "com.github.jitpack"
-version = "1.0.0"
 
 android {
     namespace = "com.demo.toast.library"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 23
@@ -33,9 +31,10 @@ android {
     }
 }
 
+
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
